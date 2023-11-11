e5ad1af281fe85812e23ca600e547800b5100d8d
giankpetrov <>
2023-11-11 20:29:56 +0000

    ADD contacto.html

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contacto.html', views.contacto, name="contacto"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     """ View to return to index page """
     return render(request, 'home.html', {})
+
+
+def contacto(request):
+    """ View to return to index page """
+    return render(request, 'contacto.html', {})
