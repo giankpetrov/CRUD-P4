2cc5a4b7a45884d7ff4c558894bc851fbcc459dc
giankpetrov <>
2023-11-17 15:43:18 +0000

    Debug True

--- booking/settings.py
+++ booking/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['giankpetrov.herokuapp.com',
                  'crud-p4-6d939b734527.herokuapp.com',
